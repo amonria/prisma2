fed0eb414c7c233e451c80eaff8f22e236646ad9
amonria <5923799+amonria@users.noreply.github.com>
2021-06-29 20:25:18 +0200

    amplication-build-ckqidr2b68288261bs6814phptn

---
+++ admin-ui/src/project/ProjectEdit.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import {
+  Edit,
+  SimpleForm,
+  EditProps,
+  DateTimeInput,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
+} from "react-admin";
+
+import { UserTitle } from "../user/UserTitle";
+
+export const ProjectEdit = (props: EditProps): React.ReactElement => {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <DateTimeInput label="Description" source="description" disabled />
+        <TextInput label="Name" multiline source="name" />
+        <ReferenceInput source="user.id" reference="User" label="Owner">
+          <SelectInput optionText={UserTitle} />
+        </ReferenceInput>
+        <DateTimeInput label="Start Date" source="startDate" />
+      </SimpleForm>
+    </Edit>
+  );
+};
