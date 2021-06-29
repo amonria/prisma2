acb4f92ab8aa8b3616b59595bdd8f6c1a3f57e62
amonria <5923799+amonria@users.noreply.github.com>
2021-06-29 20:31:37 +0200

    amplication-build-ckqidz59i8728201bs6nip5t64u

---
+++ admin-ui/src/project/ProjectCreate.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import {
+  Create,
+  SimpleForm,
+  CreateProps,
+  DateTimeInput,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
+} from "react-admin";
+
+import { UserTitle } from "../user/UserTitle";
+
+export const ProjectCreate = (props: CreateProps): React.ReactElement => {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <DateTimeInput label="Description" source="description" disabled />
+        <TextInput label="Name" multiline source="name" />
+        <ReferenceInput source="user.id" reference="User" label="Owner">
+          <SelectInput optionText={UserTitle} />
+        </ReferenceInput>
+        <DateTimeInput label="Start Date" source="startDate" />
+      </SimpleForm>
+    </Create>
+  );
+};
